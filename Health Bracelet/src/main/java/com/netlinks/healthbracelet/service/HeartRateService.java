88d0edd76350b67e624dfdf64afc7a0323f7541f
Ghassen_Benali <ghassen23@gmail.com>
2014-03-05 22:37:42 +0100

    Bluetooth Configuration Added to HeartRateService class.

--- Health Bracelet/src/main/java/com/netlinks/healthbracelet/service/HeartRateService.java
+++ Health Bracelet/src/main/java/com/netlinks/healthbracelet/service/HeartRateService.java
@@ -21,30 +21,51 @@ package com.netlinks.healthbracelet.service;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import com.netlinks.healthbracelet.MainActivity;
 import com.netlinks.healthbracelet.R;
+
+import java.io.IOException;
+import java.util.UUID;
 
 
 /**
  * Created by Saif Chaouachi on 1/28/14.
  */
 public class HeartRateService extends Service {
+    private static final String TAG = "HealthBracelet"; // Application name
+
+    private String address;    // MAC-address
+
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // Set a UUID for my device
+
+    @Override
+    public void onCreate() {
+        BluetoothConfig();
+    }
+
 
     private void showOnGoingNotification() {
         final int NOTIFICATION_ID = 1;
 
 // Create an Intent that will open the main Activity
 // if the notification is clicked.
-    //TODO replace explicit intent with implicit one
+        //TODO replace explicit intent with implicit one
         //TODO resolve multiple MainActivity launch
-       final Intent intent =
+        final Intent intent =
                 new Intent(this, MainActivity.class);
-       final PendingIntent pi = PendingIntent.getActivity(this, NOTIFICATION_ID, intent, 0);
+        final PendingIntent pi = PendingIntent.getActivity(this, NOTIFICATION_ID, intent, 0);
         // Set the Notification UI parameters
         final String name = getString(R.string.app_name);
         final NotificationCompat.Builder builder =
@@ -85,5 +106,58 @@ public class HeartRateService extends Service {
         return START_STICKY;
     }
 
+    /**
+     * Bluetooth Congiguration
+     * 1- Establishing the connection with the bracelet
+     * 2- Create the socket
+     */
+    public void BluetoothConfig() {
+        address = "BRACELET-MAC-ADDRESS"; //(String) getResources().getText(R.string.default_MAC);
 
+        btAdapter = BluetoothAdapter.getDefaultAdapter();        // Get the default Bluetooth adapter
+        // Check for Bluetooth support and then check to make sure it is turned on
+        if (btAdapter == null) {
+            Log.d(TAG, "Bluetooth is not supported");
+        } else {
+            if (btAdapter.isEnabled()) {
+                Log.d(TAG, "...Bluetooth is ON...");
+            } else {
+                Log.d(TAG, "...Bluetooth is OFF...");
+            }
+        }
+
+        Log.d(TAG, "...try connect...");
+
+
+        if (!BluetoothAdapter.checkBluetoothAddress(address)) {
+            Log.d(TAG, "Fatal Error : Incorrect MAC-address");
+        } else {
+            // Set up a pointer to the remote node using it's address.
+            BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+            // Two things are needed to make a connection:
+            //   A MAC address AND a Service UUID.
+            try {
+                btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                Log.d(TAG, "Fatal Error : socket create failed : " + e.getMessage() + ".");
+            }
+
+            // Discovery is resource intensive.  We should deactivate it
+            btAdapter.cancelDiscovery();
+
+            // Establish the connection.  This will block until it connects.
+            Log.d(TAG, "...Connecting...");
+            try {
+                btSocket.connect();
+                Log.d(TAG, "...Connection is OK...");
+            } catch (IOException e) {
+                try {
+                    btSocket.close();
+                } catch (IOException e2) {
+                    Log.d(TAG, "Fatal Error : unable to close socket during connection failure" + e2.getMessage() + ".");
+                }
+            }
+        }
+    }
 }
